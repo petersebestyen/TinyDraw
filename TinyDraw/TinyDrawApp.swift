7fcad62352fe82aa9ee40313f6db89641b852e46
Peter Sebestyen <peter.sebestyen@mac.com>
2024-09-21 11:39:50 +0200

    Initial Commit

---
+++ TinyDraw/TinyDrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyDrawApp.swift
+//  TinyDraw
+//
+//  Created by Péter Sebestyén on 2024.09.21.
+//
+
+import SwiftUI
+
+@main
+struct TinyDrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TinyDrawTests/TinyDrawTests.swift
@@ -0,0 +1,16 @@
+//
+//  TinyDrawTests.swift
+//  TinyDrawTests
+//
+//  Created by Péter Sebestyén on 2024.09.21.
+//
+
+import Testing
+
+struct TinyDrawTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
